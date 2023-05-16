4f406470a583573492ba06e358728a39f3473966
Felix D Rodriguez Perez <felixdrp@gmail.com>
2023-05-16 16:30:56 +0100

    Fix Update lib.zig unused var
    Here we’ve had to assign values to _, as Zig does not allow us to have unused values.
    https://ziglearn.org/chapter-1/#for

--- src/lib.zig
+++ src/lib.zig
@@ -4,11 +4,11 @@ const c = @import("c.zig");
 const translate = @import("translate.zig");
 
 export fn napi_register_module_v1(env: c.napi_env, exports: c.napi_value) c.napi_value {
-    _ = info;
     translate.register_function(env, exports, "greet", greet) catch return null;
     return exports;
 }
 
 fn greet(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    _ = info;
     return translate.create_string(env, "world") catch return null;
 }
